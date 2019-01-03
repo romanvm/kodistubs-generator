0f13e1bbc1e7abb4a70e9f5cd48e137ab64ccd26
Roman Miroshnychenko <roman1972@gmail.com>
2019-01-03 08:07:31 +0200

    Fix xbmcvfs.Stat method names

--- kodistubs_gen/swigparser.py
+++ kodistubs_gen/swigparser.py
@@ -212,7 +212,7 @@ def parse_function(func_doc, attributelist_tag, is_method=False):
             params.append(param)
     # Rename xbmcvfs.Stat.atime, mtime and ctime methods
     if func_doc['name'] in {'atime', 'mtime', 'ctime'}:
-        func_doc['name'] = 'cm_' + func_doc['name']
+        func_doc['name'] = 'st_' + func_doc['name']
     func_doc['signature'] = '{name}({params})'.format(
         name=func_doc['name'],
         params=', '.join(params)
